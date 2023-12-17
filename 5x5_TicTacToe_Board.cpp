47e2041549267042ae6f1dfd7d510725547b9106
Mohannad <moalsabe346@gmail.com>
2023-12-17 05:14:57 +0200

    Changed display, fixed winner-checking function

--- 5x5_TicTacToe_Board.cpp
+++ 5x5_TicTacToe_Board.cpp
@@ -7,13 +7,15 @@ _5x5_TicTacToe_Board::_5x5_TicTacToe_Board()
     board = new char *[n_rows] {};
     for (int i{}; i < n_rows; ++i)
     {
-        board[i] = new char[n_cols]{};
+        board[i] = new char[n_cols];
+        for (int j{}; j < n_cols; ++j)
+            board[i][j] = ' ';
     }
 }
 
 bool _5x5_TicTacToe_Board::update_board(int x, int y, char mark)
 {
-    if (!(x < 0 || x > 4 || y < 0 || y > 4) && (board[x + 2][y + 2] == 0))
+    if (!(x < 0 || x > 4 || y < 0 || y > 4) && (board[x + 2][y + 2] <= 'O'))
     {
         board[x + 2][y + 2] = toupper(mark);
         n_moves++;
@@ -25,72 +27,69 @@ bool _5x5_TicTacToe_Board::update_board(int x, int y, char mark)
 
 void _5x5_TicTacToe_Board::display_board()
 {
+    cout << "\n     " << 0;
+    for (int i{1}; i < 5; ++i)
+        cout << setw(6) << i;
+    cout << "\n  -------------------------------";
     for (int i : {2, 3, 4, 5, 6})
     {
-        cout << "\n| ";
+        cout << "\n"
+             << i - 2;
+        cout << " |";
         for (int j : {2, 3, 4, 5, 6})
         {
-            cout << "(" << i - 2 << "," << j - 2 << ")";
-            cout << setw(2) << board[i][j] << " |";
+            cout << setw(3) << board[i][j] << "  |";
         }
-        cout << "\n------------------------------------------";
+        cout << "\n  -------------------------------";
     }
     cout << endl;
 }
 
-bool  _5x5_TicTacToe_Board::check_horizontal(char base, short r, short c, short &counter, short i)
+bool _5x5_TicTacToe_Board::check_horizontal(char base, short r, short c, short &counter, short i)
 {
-    if (i >= 3 || board[r][c] < 1)
+    if (i >= 3 || board[r][c] == ' ')
         return false;
-    if (base == abs(board[r+i][c]))
+    if (base == (board[r][c + i]))
     {
         counter++;
         check_horizontal(base, r, c, counter, i + 1);
-        if (counter == 3)
-            board[r+i][c] = -board[r+i][c];
-        return true;
+        return counter >= 3;
     }
     return false;
 }
-bool  _5x5_TicTacToe_Board::check_vertical(char base, short r, short c, short &counter, short i)
+bool _5x5_TicTacToe_Board::check_vertical(char base, short r, short c, short &counter, short i)
 {
-    if (i >= 3 || board[r][c] < 1)
+    if (i >= 3 || board[r][c] == ' ')
         return false;
-    if (base == abs(board[r][c + i]))
+    if (base == (board[r + i][c]))
     {
         counter++;
         check_vertical(base, r, c, counter, i + 1);
-        if (counter == 3)
-            board[r][c + i] = -board[r][c + i];
-        return true;
+        return counter >= 3;
     }
     return false;
 }
-bool  _5x5_TicTacToe_Board::check_diagonal_left(char base, short r, short c, short &counter, short i)
+bool _5x5_TicTacToe_Board::check_diagonal_left(char base, short r, short c, short &counter, short i)
 {
-    if (i >= 3 || board[r][c] < 1)
+    if (i >= 3 || board[r][c] == ' ')
         return false;
-    if (base == abs(board[r + i][c + i]))
+    if (base == (board[r + i][c + i]))
     {
         counter++;
         check_diagonal_left(base, r, c, counter, i + 1);
-        if (counter == 3)
-            board[r + i][c + i] = -board[r + i][c + i];
-        return true;
+        return counter >= 3;
     }
     return false;
 }
-bool  _5x5_TicTacToe_Board::check_diagonal_right(char base, short r, short c, short &counter, short i)
+bool _5x5_TicTacToe_Board::check_diagonal_right(char base, short r, short c, short &counter, short i)
 {
-    if (i >= 3 || board[r][c] < 1)
+    if (i >= 3 || board[r][c] == ' ')
         return false;
-    if (base == abs(board[r + i][c - i]))
+    if (base == (board[r + i][c - i]))
     {
         counter++;
         check_diagonal_right(base, r, c, counter, i + 1);
-        if (counter == 3)
-            board[r + i][c - i] = -board[r + i][c - i];
-        return true;
+        return counter >= 3;
     }
     return false;
 }
@@ -100,54 +99,54 @@ bool _5x5_TicTacToe_Board::is_winner()
     if (n_moves < 24)
         return false;
     short counter{};
-    for (int i{2}; i < n_rows; ++i)
+    for (int i{2}; i < n_rows - 2; ++i)
     {
-        for (int j{2}; j < n_cols; ++j)
+        for (int j{2}; j < n_cols - 2; ++j)
         {
-            if(board[i][j] == 'X')
+            if (board[i][j] == 'X')
             {
-                _3Xs += check_diagonal_left(board[i][j],i,j,counter,-2);
+                _3Xs += check_diagonal_left(board[i][j], i, j, counter, -2);
             }
             else
             {
-                _3Os += check_diagonal_left(board[i][j],i,j,counter,-2);
+                _3Os += check_diagonal_left(board[i][j], i, j, counter, -2);
             }
             counter = 0;
-            if(board[i][j] == 'X')
+            if (board[i][j] == 'X')
             {
-                _3Xs += check_diagonal_right(board[i][j],i,j,counter,-2);
+                _3Xs += check_diagonal_right(board[i][j], i, j, counter, -2);
             }
             else
             {
-                _3Os += check_diagonal_right(board[i][j],i,j,counter,-2);
+                _3Os += check_diagonal_right(board[i][j], i, j, counter, -2);
             }
             counter = 0;
-            if(board[i][j] == 'X')
+            if (board[i][j] == 'X')
             {
-                _3Xs += check_vertical(board[i][j],i,j,counter,-2);
+                _3Xs += check_vertical(board[i][j], i, j, counter, -2);
             }
             else
             {
-                _3Os += check_vertical(board[i][j],i,j,counter,-2);
+                _3Os += check_vertical(board[i][j], i, j, counter, -2);
             }
             counter = 0;
-            if(board[i][j] == 'X')
+            if (board[i][j] == 'X')
             {
-                _3Xs += check_horizontal(board[i][j],i,j,counter,-2);
+                _3Xs += check_horizontal(board[i][j], i, j, counter, -2);
             }
             else
             {
-                _3Os += check_horizontal(board[i][j],i,j,counter,-2);
+                _3Os += check_horizontal(board[i][j], i, j, counter, -2);
             }
             counter = 0;
         }
     }
-    if(_3Xs > _3Os)
+    if (_3Xs > _3Os)
     {
         winner = 'X';
         return true;
     }
-    else if(_3Os > _3Xs)
+    else if (_3Os > _3Xs)
     {
         winner = 'O';
         return true;
